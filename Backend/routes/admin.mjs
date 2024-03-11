24fbffafb2449a1be27fb7b997f70e913ba38bd1
Suman Giri <0107sumangiri@gmail.com>
2024-03-11 12:37:30 +0545

    modified

--- Backend/routes/admin.mjs
+++ Backend/routes/admin.mjs
@@ -146,6 +146,12 @@ res.status(200).json({courses});
   }
  })
 
+ router.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({
+      error: 'An unexpected error occurred.'
+  });
+});
 
 
 
--- Backend/routes/user.mjs
+++ Backend/routes/user.mjs
@@ -161,5 +161,13 @@ router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     }
 });
 
+router.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({
+        error: 'An unexpected error occurred.'
+    });
+});
+
+
 export default router;
 
